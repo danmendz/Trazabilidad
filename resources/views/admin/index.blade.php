655addc9819d372ee5d615008c90390d0dec00cc
Trinidad9 <157648645+Trinidad9@users.noreply.github.com>
2024-06-10 15:10:59 -0600

    test

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <h1>Bienvenido, Administrador</h1>
+    <h1>Bienvenido, Administrador :</h1>
     <p>Este es el panel de administración.</p>
     <!-- Aquí puedes agregar más contenido específico para administradores -->
 </div>
